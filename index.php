f68ed52def21e395c2add072a61716db66d4046c
reyzakertopro <72240571+reyzakertopro@users.noreply.github.com>
2024-10-02 17:06:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>Mitra Industrial Chrome Teknologi | The Total Solution Repair</title>
+	<meta name="description" content="PT. Mitra Industrial Chrome Teknologi (MICT) adalah perusahaan swasta yang bergerak dibidang Hard Chrome Platting, Hydraulics, Pneumatics, Roll, Rekondisi Valve, Manufacturing, Engineering Service dan Enggine Service yang berlokasi kota Tangerang, Banten, Indonesia." />
+	<link rel="canonical" href="https://ptmict.id/" />
+	<meta property="og:locale" content="id_ID" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="PT Mitra Industrial Chrome Teknologi | The Total Solution Repair" />
+	<meta property="og:description" content="PT. Mitra Industrial Chrome Teknologi (MICT) adalah perusahaan swasta yang bergerak dibidang Hard Chrome Platting, Hydraulics, Pneumatics, Roll, Rekondisi Valve, Manufacturing, Engineering Service dan Enggine Service yang berlokasi kota Tangerang, Banten, Indonesia." />
+	<meta property="og:url" content="https://ptmict.id/" />
+	<meta property="og:site_name" content="PT Mitra Industrial Chrome Teknologi" />
+	<meta property="article:modified_time" content="2023-09-05T13:21:34+00:00" />
+	<meta property="og:image" content="" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<script type="application/ld+json" class="yoast-schema-graph">{"@context":"https://schema.org","@graph":[{"@type":"WebPage","@id":"https://ptmict.id/","url":"https://ptmict.id/","name":"Web Resmi PT Mitra Industrial Chrome Teknologi | The Total Solution Repair","isPartOf":{"@id":"https://ptmict.id/#website"},"about":{"@id":"https://ptmict.id/#organization"},"primaryImageOfPage":{"@id":"https://ptmict.id/#primaryimage"},"image":{"@id":"https://ptmict.id/#primaryimage"},"thumbnailUrl":"https://ptmict.id/wp-content/uploads/2023/09/09-.jpg","datePublished":"2019-10-07T03:11:24+00:00","dateModified":"2023-09-05T13:21:34+00:00","description":"PT. Mitra Industrial Chrome Teknologi (MICT) adalah perusahaan swasta yang bergerak dibidang Hard Chrome Platting, Hydraulics, Pneumatics, Roll, Rekondisi Valve, Manufacturing, Engineering Service dan Enggine Service yang berlokasi kota Tangerang, Banten, Indonesia.","breadcrumb":{"@id":"https://ptmict.id/#breadcrumb"},"inLanguage":"id","potentialAction":[{"@type":"ReadAction","target":["https://ptmict.id/"]}]},{"@type":"ImageObject","inLanguage":"id","@id":"https://ptmict.id/#primaryimage","url":"https://ptmict.id/wp-content/uploads/2023/09/09-.jpg","contentUrl":"https://ptmict.id/wp-content/uploads/2023/09/09-.jpg","width":2508,"height":1597,"caption":"Repair"},{"@type":"BreadcrumbList","@id":"https://ptmict.id/#breadcrumb","itemListElement":[{"@type":"ListItem","position":1,"name":"Home"}]},{"@type":"WebSite","@id":"https://ptmict.id/#website","url":"https://ptmict.id/","name":"PT Mitra Industrial Chrome Teknologi","description":"The Total Solution Repair","publisher":{"@id":"https://ptmict.id/#organization"},"potentialAction":[{"@type":"SearchAction","target":{"@type":"EntryPoint","urlTemplate":"https://ptmict.id/?s={search_term_string}"},"query-input":"required name=search_term_string"}],"inLanguage":"id"},{"@type":"Organization","@id":"https://ptmict.id/#organization","name":"PT Mitra Industrial Chrome Teknologi","url":"https://ptmict.id/","logo":{"@type":"ImageObject","inLanguage":"id","@id":"https://ptmict.id/#/schema/logo/image/","url":"https://ptmict.id/wp-content/uploads/2019/10/logo-mict-web-e1570273553741.png","contentUrl":"https://ptmict.id/wp-content/uploads/2019/10/logo-mict-web-e1570273553741.png","width":150,"height":93,"caption":"PT Mitra Industrial Chrome Teknologi"},"image":{"@id":"https://ptmict.id/#/schema/logo/image/"}}]}</script>
+	<meta name="google-site-verification" content="rcvlWLZXWGosFJD9oHR5i2Em4DBZ23lZDL_EDMA2ovY" />
+
+  <!-- Author -->
+  <meta name="Reyza Kertoprodjo" content="Mitra Industrial Chrome Teknologi">
+  <!-- keywords -->
+  <meta name="keywords" content="Mitra Industrial Chrome Teknologi, MICT, Hydraulics repair, Hydrulics tangerang">
+
+  <link rel="icon" type="image/x-icon" href="assets/img/logoMICT.png">
+
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+  <!-- FontAwesome Icons-->
+  <script src="https://kit.fontawesome.com/1020a358ce.js" crossorigin="anonymous"></script>
+
+  <!-- Master Style -->
+  <link rel="stylesheet" href="assets/css/master.css">
+
+</head>
+
+<body>
+  <button class="whatsapp" type="button"><i class="fa-brands fa-whatsapp"></i></button>
+  <nav>
+    <div class="wrapper">
+      <a href=""><span class="logo"><img src="assets/img/logoMICT.png" alt="MICT_logo"></span></a>
+      <div class="nav-menu">
+        <a href="#"><span>Projects</span></a>
+        <a href="#"><span>About</span></a>
+        <a href="#"><span>Product & Services</span></a>
+        <a href="#"><span>Berita</span></a>
+        <a href="#"><span>Login</span></a>
+
+      </div>
+      <button type="button" id="hamburger"><i class="fa-solid fa-bars"></i></button>
+
+    </div>
+
+  </nav>
+
+  <main>
+    <section class="hero">
+      <span class="headline">Hard Chrome Platting, Hydraulics, Pneumatics, Roll, Rekondisi Valve, Manufacturing dan Engineering Service</span>
+      <hr>
+      <span class="caption">Kawasan Kavling DPR Blok A No. 153 Kenanga, Cipondoh Kota Tangerang, Banten - Indonesia 151446</span>
+      <a href="#" class="hero-cta"><span>Hubungi Kami</span></a>
+
+    </section>
+
+    <div class="section-wrapper">
+      <section class="partner">
+        <span class="judul">Beberapa Partner Kami</span>
+        <div class="partner-wrapper">
+          <img src="assets/img/samudra.png" alt="Samudra">
+          <img src="assets/img/nestle.png" alt="Nestle">
+          <img src="assets/img/sucaco.png" alt="Sucaco">
+          <img src="assets/img/tanto.png" alt="Tanto">
+          <img src="assets/img/dynaplast.png" alt="Dynaplast">
+          <img src="assets/img/pelindo.png" alt="Pelindo">
+
+        </div>
+
+      </section>
+
+      <section class="services">
+        <div class="wrapper">
+          <div style="display: flex; flex-direction: column; gap: .75em;">
+            <span class="judul">Layanan Kami</span>
+            <span class="caption">seluruh pekerjaan kami dilakukan oleh ahli di bidangnya didukung dengan bengkel terbaik dan dilengkapi dengan peralatan yang lengkap dan memadai</span>
+            <a href="#" class="service-cta">Lihat lebih lengkap</a>
+
+          </div>
+          <div class="card-wrapper">
+            <div class="card">
+              <span><i class="fa-solid fa-gears"></i></span>
+              <span class="card-judul">Manufacture unit hydraulic baru</span>
+              <span class="card-caption">Proses kustomisasi khusus, dapat disesuaikan dengan perubahan kebutuhan di masa depan</span>
+
+            </div>
+            <div class="card">
+              <span><i class="fa-solid fa-screwdriver-wrench"></i></span>
+              <span class="card-judul">Perbaikan</span>
+              <span class="card-caption">Reparasi hydraulic unit, cylinder tube, seal replacement dan lainnya sesuai dengan kebutuhan perbaikan</span>
+
+            </div>
+            <div class="card">
+              <span><i class="fa-solid fa-weight-hanging"></i></span>
+              <span class="card-judul">Hard chrome plating</span>
+              <span class="card-caption">Pelapisan logam dengan lapisan chrome yang tebal dan kuat. Hasilnya adalah lapisan yang lebih keras, lebih tahan aus, dan lebih tahan terhadap korosi</span>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </section>
+
+      <section class="case-projects">
+        <span class="judul">Project Case</span>
+        <div class="wrapper">
+          <div class="case-card">
+            <div class="img-container">
+              <img src="assets/img/indah kiat pulp & paper perawang before.png" alt="case1_before">
+              <span>Before</span>
+
+            </div>
+            <div class="img-container">
+              <img src="assets/img/indah kiat pulp & paper perawang after.png" alt="case1_after">
+              <span>After</span>
+
+            </div>
+            <div class="desc">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <br>Cylinder unit repair : PT. Indah Kiat Pulp & Paper Perawang</p>
+                <a href="#" class="case-CTA"><span>Lihat lebih lanjut</span></a>
+
+            </div>
+
+          </div>
+
+          <div class="case-card">
+            <div class="desc" style="flex-grow: 1;">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                <br>PT. Tangguh Samudra Jaya</p>
+                <a href="#" class="case-CTA"><span>Lihat lebih lanjut</span></a>
+
+            </div>
+
+            <div class="img-gallery" style="flex-grow: 2;">
+              <img src="assets/img/samudra_case1.png" alt="case1_samudra">
+              <img src="assets/img/samudra_case2.png" alt="case2_samudra">
+              <img src="assets/img/samudra_case3.png" alt="case3_samudra">
+              <img src="assets/img/samudra_case4.png" alt="case4_samudra">
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </section>
+
+      <section class="blog">
+        <div class="wrapper">
+          <span class="judul">Update terkini</span>
+          <div>
+            <a href="#">
+              <div class="blog-card" style="background: linear-gradient(180deg, rgba(1, 1, 3, .15), rgba(1, 1, 3, .5)), url('assets/img/hero_banner1.png') center center / cover;">
+                <span class="blog-judul">Berita1</span>
+                <span class="blog-tanggal">27 September, 2024</span>
+
+              </div>
+            </a>
+
+            <a href="#">
+              <div class="blog-card" style="background: linear-gradient(180deg, rgba(1, 1, 3, .15), rgba(1, 1, 3, .5)), url('assets/img/hero_banner1.jpg') center center / cover;">
+                <span class="blog-judul">Berita2</span>
+                <span class="blog-tanggal">30 September, 2024</span>
+
+              </div>
+            </a>
+
+            <a href="#">
+              <div class="blog-card" style="background: linear-gradient(180deg, rgba(1, 1, 3, .15), rgba(1, 1, 3, .5)), url('assets/img/hero_banner2.jpg') center center / cover;">
+                <span class="blog-judul">Berita3</span>
+                <span class="blog-tanggal">1 Oktober, 2024</span>
+
+              </div>
+            </a>
+
+            <a href="#">
+              <div class="blog-card" style="background: linear-gradient(180deg, rgba(1, 1, 3, .15), rgba(1, 1, 3, .5)), url('assets/img/jason-jarrach-_CVuT8IBtjY-unsplash.jpg') center center / cover;">
+                <span class="blog-judul">Berita4</span>
+                <span class="blog-tanggal">1 Oktober, 2024</span>
+
+              </div>
+            </a>
+
+          </div>
+
+        </div>
+
+      </section>
+
+    </div>
+
+  </main>
+
+  <style>
+    footer .wrapper{
+      display: flex;
+      gap: 1.75em;
+    }
+    footer .wrapper div{
+      padding: 1.25em 0;
+      flex-direction: column;
+      display: flex;
+      gap: .5em;
+      position: relative;
+    }
+    footer .wrapper div .judul{
+      font-size: 1.15em;
+      font-weight: 600;
+    }
+    .footer-loc iframe{max-height: 240px;}
+
+    @media(max-width: 992px) {
+      footer{justify-content: center;}
+      footer .wrapper{flex-direction: column;}
+      footer .wrapper div{
+        align-items: center;
+      }
+      footer .wrapper div a{
+        display: flex;
+        justify-content: center;
+      }
+      footer .wrapper div span{text-align: center;}
+      footer .wrapper div:not(:nth-last-child(1)) span:nth-last-child(1):after{
+        content: '';
+        position: absolute;
+        background: rgba(255, 255, 255, .25);
+        width: 100%;
+        height: 1px;
+        bottom: 0;
+        left: 0;
+      }
+
+    }
+
+  </style>
+
+  <footer>
+    <div class="wrapper">
+      <div class="footer-desc">
+        <a href="#"><span class="judul">Mitra Indutrial Chrome Teknologi</span></a>
+        <span>Kawasan Kavling DPR</span>
+        <span>Blok A No. 153 Kenanga, Cipondoh Tangerang, 15146</span>
+        <span>Banten Indonesia</span>
+        <span>Telepon: (021) 22263653</span>
+        <a href="mailto:office@ptmict.id?subject=MICT Support&body=Hallo MICT">
+          <span>Email: office@ptmict.id</span>
+        </a>
+
+      </div>
+
+      <div class="footer-menu">
+        <span class="judul">Products & Services</span>
+        <a href="#"><span>Engineering Design & Development</span></a>
+        <a href="#"><span>Manufacture</span></a>
+        <a href="#"><span>Hard Chrome Platting</span></a>
+        <a href="#"><span>Ceramic Coating</span></a>
+
+      </div>
+
+      <div class="footer-loc">
+        <span class="judul">Our Workshop</span>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.5221063832496!2d106.6868403!3d-6.1946287999999985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f9a4c10ac315%3A0x91d17ca3b59e1175!2sMitra%20Industrial%20Chrome%20Teknologi%20Pt.%2C%20Jl.%20Bulak%20Kinung%20Kav.%20Dpr%20Blok%20A%20No.153%2C%20RT.004%2FRW.003%2C%20Kenanga%2C%20Kec.%20Cipondoh%2C%20Kota%20Tangerang%2C%20Banten%2015146!5e0!3m2!1sen!2sid!4v1727863190388!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+
+      </div>
+
+    </div>
+
+    <span style="text-align: center;">© <?= date('Y'); ?> Mitra Industrial Chrome Teknologi. All rights reserved.</span>
+
+  </footer>
+
+</body>
+
+<script src="assets/js/navbar.js"></script>
+
+</html>
